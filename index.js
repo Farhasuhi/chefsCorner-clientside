70cd340c5305471d20d65629dec72e6fd5f0083b
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-05-02 22:58:24 +0600

    add chefs and recipes json file

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app=express();
 const cors=require('cors');
 app.use(cors())
 const port=process.env.PORT || 5000;
+const chefs=require('./data/chefs.json');
+const recipes=require('./data/recipes.json')
 
 app.get('/',(req,res)=>{
     res.send('chefs corner server site')
+})
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
+app.get('/recipes',(req,res)=>{
+    res.send(recipes)
 })
 
 app.listen(port,()=>{
